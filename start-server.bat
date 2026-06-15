1a4f9cd04f95586af975d32195c2c0a104310c9d
AI Auto-Updater <updater@libraryofailexandria.app>
2026-06-15 23:07:13 +0200

    Strip repo to only source code

--- start-server.bat
+++
@@ -1,9 +0,0 @@
-call update-plugins.bat
-
-:loop
-@echo off
-echo [%date% %time%] Starting Shadowfang server...
-"java\jdk-25.0.1+8\bin\java.exe" -Xms2G -Xmx2G -jar folia.jar nogui
-echo [%date% %time%] Server stopped. Restarting in 5 seconds...
-timeout /t 5 /nobreak >nul
-goto loop
